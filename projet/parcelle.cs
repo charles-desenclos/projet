8c780efaab3f5656590fb2bfab17fe1f4e18e49b
charles-desenclos <85804366+charles-desenclos@users.noreply.github.com>
2021-06-12 20:52:16 +0200

    Add files via upload

---
+++ projet/parcelle.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace projet
+{
+    class parcelle
+    {
+        int parcelles;
+        public parcelle (int P )
+        {
+            parcelles = P;
+            if (parcelles > 4)
+            {
+                Console.WriteLine("Parcelle a : {0} unites", parcelles);
+            }
+        }
+    }
+}
